7f03a4f12b6c1ef4c92d03ce53e8c32edf5e3e7a
qiansc <van3008@163.com>
2019-11-23 18:01:33 +0800

    feat: support match mode && backward reference

--- src/rule.ts
+++ src/rule.ts
@@ -1,13 +1,22 @@
 import { Context } from './context'
 import { Recipe } from './recipe'
-import { makeRe } from 'minimatch'
 import debugFactory from 'debug'
+const extglob = require('extglob');
 
 const isGlob = require('is-glob')
 const debug = debugFactory('makit:rule')
 
 type prerequisitesResolver = (context: Context) => (string[] | string | Promise<string | string[]>)
 type prerequisitesItem = string | prerequisitesResolver
+enum PrerequisiteMode {
+    Match,
+    Static,
+    String,
+    StringArray,
+    Function,
+    FunctionArray
+}
+
 
 export type TargetDeclaration = string | RegExp
 export type PrerequisitesDeclaration = string | prerequisitesResolver | (string | prerequisitesResolver)[]
@@ -24,6 +33,7 @@ export class Rule {
 
     private rTarget: RegExp
     private prerequisites: PrerequisitesDeclaration
+    private prerequisiteMode: PrerequisiteMode;
 
     constructor (
         target: TargetDeclaration,
@@ -31,9 +41,36 @@ export class Rule {
         recipe: Recipe
     ) {
         this.target = target
+
         this.prerequisites = prerequisites
+        if (typeof prerequisites === "string") {
+            if (prerequisites.match(/\$\d/)) {
+                this.prerequisiteMode = PrerequisiteMode.Match
+            } else {
+                this.prerequisiteMode = PrerequisiteMode.String
+            } 
+        } else if (Array.isArray(prerequisites)){
+            if (typeof(prerequisites[0]) === 'string') {
+                this.prerequisiteMode = PrerequisiteMode.StringArray
+            } else if (typeof(prerequisites[0]) === 'function') {
+                this.prerequisiteMode = PrerequisiteMode.FunctionArray
+            }
+        } else {
+            this.prerequisiteMode = PrerequisiteMode.Function
+        }
+
         this.recipe = recipe
-        this.rTarget = typeof target === 'string' ? makeRe(target) : target
+        if (typeof target === 'string') {
+            if (target.indexOf('$') == -1) {
+                // Matching Mode
+                this.rTarget = new RegExp(extglob(target).replace(/\(\?\:/g, '('))
+            } else {
+                // Support Backward reference
+                this.rTarget = extglob.makeRe(target)
+            }
+        } else {
+            this.rTarget = target
+        }
         this.targetType = target instanceof RegExp
             ? TargetType.regexp
             : (
@@ -46,31 +83,52 @@ export class Rule {
     }
 
     public getPrerequisites (context: Context) {
-        return getPrerequisitesFromDeclaration(context, this.prerequisites)
+        return this.getPrerequisitesFromDeclaration(context, this.prerequisites)
     }
 
     public match (target: string) {
         debug('matching', target, 'against', this.rTarget)
         return this.rTarget.exec(target)
     }
+
+    public async getPrerequisitesFromDeclaration (ctx: Context, decl: PrerequisitesDeclaration) {
+        switch(this.prerequisiteMode) {
+            case PrerequisiteMode.Match:
+                decl = (decl as string).replace(/\$(\d+)/g, (all, index) => {
+                    return ctx.match[index];
+                });
+                decl = [decl]
+                break;
+            case PrerequisiteMode.String:
+                decl = [decl as string]
+                break;
+            case PrerequisiteMode.Function:
+                decl = [decl as prerequisitesResolver]
+                break;
+            case PrerequisiteMode.FunctionArray:
+                decl = decl as prerequisitesResolver[]
+                break
+            case PrerequisiteMode.StringArray:
+                decl = decl as string[]
+                break;
+            default:
+                return [];
+        }
+
+        const result = []
+        for (const dependency of decl) {
+            if (typeof dependency === 'function') {
+                const deps = await dependency(ctx)
+                if (Array.isArray(deps)) {
+                    result.push(...deps)
+                } else {
+                    result.push(deps)
+                }
+            } else {
+                result.push(dependency)
+            }
+        }
+        return result
+    }
 }
 
-async function getPrerequisitesFromDeclaration (ctx: Context, decl: PrerequisitesDeclaration) {
-    if (!Array.isArray(decl)) {
-        decl = [decl]
-    }
-    const result = []
-    for (const dependency of decl) {
-        if (typeof dependency === 'function') {
-            const deps = await dependency(ctx)
-            if (Array.isArray(deps)) {
-                result.push(...deps)
-            } else {
-                result.push(deps)
-            }
-        } else {
-            result.push(dependency)
-        }
-    }
-    return result
-}
---
+++ test/e2e/pattern.spec.ts
@@ -0,0 +1,22 @@
+import { Makefile } from '../../src/index'
+import { removeSync, readFileSync } from 'fs-extra'
+const md5 = require('md5')
+
+describe('glob', function () {
+    const output0 = 'test/e2e/d.md5.out';
+    beforeEach(() => removeSync(output0))
+    const sum = '89a73e70b37e4631dab23c408b18be32'
+
+    it('should support matchMode', async function () {
+        const mk = new Makefile(process.cwd())
+
+        const recipe = async function () {
+            return this.writeTarget(md5(await this.readDependency()))
+        }
+        mk.addRule('(test/**)/(*).md5.out', '$1/$2.san.js', recipe)
+
+        await mk.make(output0)
+        expect(readFileSync(output0, 'utf8')).toEqual(sum)
+    })
+
+})
