0b524b1520ac805ebd8bebec11b87d6f1996c53f
harttle <yangjvn@126.com>
2021-02-26 20:05:40 +0800

    fix: support node <= 10

--- src/fs/nodefs-impl.ts
+++ src/fs/nodefs-impl.ts
@@ -1,9 +1,18 @@
 import { FileSystem } from './file-system'
+import { promisify } from 'util'
 import * as fs from 'fs'
+
+const stat = promisify(fs.stat)
+const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
+const mkdir = promisify(fs.mkdir)
+const unlink = promisify(fs.unlink)
+const exists = promisify(fs.exists)
+const utimes = promisify(fs.utimes)
 
 export class NodeFileSystem implements FileSystem {
     stat (path: string): Promise<fs.Stats> {
-        return fs.promises.stat(path)
+        return stat(path)
     }
     statSync (path: string): fs.Stats {
         return fs.statSync(path)
@@ -11,7 +20,7 @@ export class NodeFileSystem implements FileSystem {
 
     readFile (path: string, encoding?: string): Promise<string>
     readFile (path: string, encoding?: string): Promise<string | Buffer> {
-        return fs.promises.readFile(path, encoding)
+        return readFile(path, encoding)
     }
 
     readFileSync (path: string, encoding: string): string;
@@ -20,21 +29,21 @@ export class NodeFileSystem implements FileSystem {
     }
 
     writeFile (path: string, data: string): Promise<void> {
-        return fs.promises.writeFile(path, data)
+        return writeFile(path, data)
     }
     writeFileSync (path: string, data: string): void {
         return fs.writeFileSync(path, data)
     }
 
     mkdir (path: string, options?: number | string | fs.MakeDirectoryOptions | null): Promise<void> {
-        return fs.promises.mkdir(path, options)
+        return mkdir(path, options)
     }
     mkdirSync (path: string, options?: number | string | fs.MakeDirectoryOptions | null): void {
         return fs.mkdirSync(path, options)
     }
 
     unlink (path: string): Promise<void> {
-        return fs.promises.unlink(path)
+        return unlink(path)
     }
     unlinkSync (path: string) {
         return fs.unlinkSync(path)
@@ -54,7 +63,7 @@ export class NodeFileSystem implements FileSystem {
     }
 
     utimes (path: string, atime: number, mtime: number): Promise<void> {
-        return fs.promises.utimes(path, atime, mtime)
+        return utimes(path, atime, mtime)
     }
     utimesSync (path: string, atime: number, mtime: number) {
         return fs.utimesSync(path, atime, mtime)
